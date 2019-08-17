fae9dddace3109189a3cf48d56a5df37527404c9
Adriel da Silva <silvaadriel510@gmail.com>
2019-08-17 00:16:41 -0300

    cors added

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 const server = express();
 
@@ -9,6 +10,7 @@ mongoose.connect(
   { useNewUrlParser: true }
 );
 
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 
