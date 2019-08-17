cb27131c3a19421ec68d7ed17180bc22359af323
Adriel da Silva <silvaadriel510@gmail.com>
2019-08-16 22:09:14 -0300

    mongodb connection

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const server = express();
+
+mongoose.connect(
+  'mongodb+srv://silvaadriel:udj9kxIxx1yorVlg@cluster0-pkwby.mongodb.net/tindev?retryWrites=true&w=majority',
+  { useNewUrlParser: true }
+);
 
 server.use(express.json());
 server.use(routes);
